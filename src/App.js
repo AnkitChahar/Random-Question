687733674f01a7a6c3e1029615092b6c2f9bd922
Ankit <ankitchahar11@gmail.com>
2019-12-15 21:09:54 +0530

    Added HTTPS API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-
-  const problemSetURL = "http://codeforces.com/api/problemset.problems"
+  const problemSetURL =
+    "https://codeforces.com/api/problemset.problems";
 
   const [randomProblem, setRandomProblem] = useState({});
 
   let problemData = null;
+
   useEffect(() => {
-    axios.get(problemSetURL)
-    .then(res => {
-      console.log(res.data.status)
-      problemData = res.data.result.problems
+    if (problemData == null) {
+      console.log("Fetching Entire Data!")
+      axios.get(problemSetURL)
+        .then(res => {
+          console.log(res.data.status);
+          problemData = res.data.result.problems;
+          getRandomQuestion();
+        }
+        );
+    }
+  }, []);
+
+  const getRandomQuestion = () => {
+    if (problemData != null) {
       setRandomProblem(problemData[Math.round(Math.random() * Object.keys(problemData).length)]);
-    })
-  },[]);
+    }
+  };
 
   return (
-    <div className="App">
-      <h1>{randomProblem.contestId}</h1>
-      <h1>{randomProblem.name}</h1>
-      <a target="_blank" rel="noopener noreferrer" href={"http://codeforces.com/problemset/problem/"+randomProblem.contestId+"/"+randomProblem.index}>Click Here to go to the page!</a>
+    <div className={Object.keys(randomProblem).length === 0 ? "hidden" : "visibility"}>
+      <p>
+        {randomProblem.contestId}
+      </p>
+      <p>
+        {randomProblem.name}
+      </p>
+      <a target="_blank" rel="noopener noreferrer" href={`http://codeforces.com/problemset/problem/${randomProblem.contestId}/${randomProblem.index}`}>
+        Click Here to go to the page!
+        </a>
     </div>
   );
 }
